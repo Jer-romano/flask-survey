75e3de1926b27a8d07727be9a564eea1558ef9b3
Jeremy Romano <jar315@pitt.edu>
2023-06-11 21:33:43 -0400

    Added comments

--- app.py
+++ app.py
@@ -10,11 +10,13 @@ responses = []
 questions_answered_count = 0
 @app.route("/")
 def show_homepage():
+    '''Displays homepage where user starts the survey'''
     return render_template("base.html", title=satisfaction_survey.title,
     instructions=satisfaction_survey.instructions)
 
 @app.route("/questions/<int:q_number>")
 def show_question(q_number):
+    '''Displays a question page. Also includes logic to prevent URL tampering.'''
     if q_number != questions_answered_count:
         flash("Error: You're attempting to access an invalid question.", "error")
         return redirect(f"/questions/{questions_answered_count}")
@@ -26,6 +28,8 @@ def show_question(q_number):
 
 @app.route("/answer", methods=["POST"])
 def handle_answer():
+    '''Called when user submits an answer. Answer is added to responses list. Redirects
+    to the next question'''
     global questions_answered_count
     questions_answered_count += 1
     answer = request.form["options"]
@@ -35,4 +39,5 @@ def handle_answer():
 
 @app.route("/thanks")
 def say_thanks():
+    '''Displays page thanking user for completing the survey'''
     return render_template("thanks.html", title=satisfaction_survey.title)
